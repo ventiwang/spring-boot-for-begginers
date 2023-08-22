a6521c98292b7309a7377396de0b2fc892a1854a
venti <venti@mail.com>
2023-08-23 07:41:09 +0800

    initializing spring boot project

---
+++ src/test/java/com/venti/sprintDemo/SprintDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venti.sprintDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
